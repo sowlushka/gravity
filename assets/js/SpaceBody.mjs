2a5e3439c7804c7344b8e7f3aaa5a38ace620092
Anastasiya Melnikova (work) <sav20043@yandex.ru>
2023-10-02 15:52:44 +0300

    Подготовка класса SpaceBody Инициализация объектов Солнце, Земля, Луна

--- assets/js/SpaceBody.mjs
+++ assets/js/SpaceBody.mjs
@@ -1,23 +1,33 @@
 //Класс космического тела
-
+import {G, dt} from "./constants.mjs";
 
 export class SpaceBody{
     name;//Имя космического тела
-    m; //Масса
+    #m; //Масса
     
-    r=[];//Массив координат тела x,y,z
     
-    v=[];//Вектор скорости по направлениям x,y,z
+    #r=[];//Массив координат тела x,y,z
+    #newR=[];//Массив новых расчётных координат тела через время дифференцирования dt
+
+    
+    #v=[];//Вектор скорости по направлениям x,y,z
+    #newV=[];//Массив новых расчётных значений скорости через время дифференцирования dt
     
     constructor({name, m, x,y,z=0, vx, vy, vz=0}){
-        this.m=m;
+        this.#m=m;
         this.name=name;
-        this.r[0]=x;
-        this.r[1]=y;
-        this.r[2]=z;
-        this.v[0]=vx;
-        this.v[1]=vy;
-        this.v[2]=vz;
+        this.#r[0]=x;
+        this.#r[1]=y;
+        this.#r[2]=z;
+        this.#v[0]=vx;
+        this.#v[1]=vy;
+        this.#v[2]=vz;
     }
+
+    get m(){
+        return this.#m;
+    }
+
+    
 
 }
--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -5,4 +5,14 @@ import * as cnst from "./constants.mjs";
 
 let bodyes=[];//Массив небесных тел
 
+const bSun=new SpaceBody({name:"Солнце", m: cnst.mSun, x:0, y:0, vx:0, vy:0});
 const bEarth=new SpaceBody({name:"Земля", m: cnst.mEarth, x:1, y:0, vx:0, vy: cnst.vEarth});
+const bMoon=new SpaceBody({     name: "Луна", 
+                                m: cnst.mMoon, 
+                                x:1, 
+                                y: cnst.rMoon, 
+                                vx: cnst.vMoon,
+                                vy: 0});
+
+
+console.log(bSun.m);
